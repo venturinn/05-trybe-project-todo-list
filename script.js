63a68615ff68fd255170e8078ce8b517c259f397
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-23 18:26:18 -0300

    Adicionado requisito 07.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const textoTarefa = document.getElementById('texto-tarefa');
 const lista = document.getElementsByTagName('ol');
 const quadroCentral = document.getElementById('quadro-central');
 
-
 // Função para criar os itens da lista de tarefas
 
 function creat() {
@@ -12,9 +11,22 @@ function creat() {
   text.innerText = textoTarefa.value;
   text.style.height = '20px';
   text.style.marginLeft = '15px';
+  text.style.lineHeight= '20px';
   lista[0].appendChild(text);
   textoTarefa.value = '';
-  quadroCentral.style.height = `${String(quadroCentral.offsetHeight + 20)}px`; 
+  quadroCentral.style.height = `${String(quadroCentral.offsetHeight + 20)}px`;
 }
 
 criarTarefa.addEventListener('click', creat);
+
+// Função para colocar cor ao item da lista clicado
+
+function liColor(event) {
+  const liClicada = event.target;
+
+  if (liClicada.tagName === 'LI') {
+    liClicada.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
+document.addEventListener('click', liColor);
